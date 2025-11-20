4b5f5d1a6b50234b8275cfff3946e0bd2866f8c8
Keltoum Malouki <keltoummalouki@gmail.com>
2025-11-20 10:59:23 +0100

    Frontend Fix import Index

--- frontend/src/lib/main.tsx
+++ frontend/src/lib/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "../App.tsx";
-import "./index.css";
+import "../index.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
